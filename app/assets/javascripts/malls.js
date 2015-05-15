da78c5494ed2955fdc4912d540e55588c0becb05
Willy Ko <willyko@y7mail.com>
2015-05-15 01:40:08 -0700

    working copy before adding user base

--- app/assets/javascripts/malls.js
+++ app/assets/javascripts/malls.js
@@ -3,39 +3,19 @@ $(document).ready(function () {
     $(this).siblings('.form-control').toggle();
   });
 
-  $('#selection1').hide();
-  $('#selection2').hide();
+  $('.list-block').on('click', '.mall-name', function () {
+    $(this).siblings('.search-bar').slideToggle();
+  });
 
-  //$('.mall-name').mouseleave( function () {
-  //  $(this).children('#selection').remove();
-  //  $(this).children('h2').show();
-  //});
-  $('li').mouseenter( function () {
+
+  $('.list-block').on('mouseenter', 'li',  function () {
     $(this).css('background-color' , 'LightGray');
   });
 
-  $('li').mouseleave( function () {
+  $('.list-block').on('mouseleave', 'li', function () {
     $(this).css('background-color' , '#222222');
   });
 
-  //$('#selection1').autocomplete({
-  //  minLength: 0,
-  //  source: "malls",
-  //  focus: function(event, ui) {
-  //    $('selection1').val(ui.item.mall.name);
-  //    return false;
-  //  },
-  //  select: function(event, ui) {
-  //    $('selection1').val(ui.item.mall.name);
-  //    return false;
-  //  }
-  //}).autocomplete("instance")._renderItem = function(ul, item) {
-  //    return $( "<li>" )
-  //      .data( "ui-autocomplete-item", item)
-  //      .append( "<a>" + item.mall.name + "</a>" )
-  //      .appendTo( ul );
-
-  //  };
 
   $('#filter-list').keyup(function (e) {
     if(e.keyCode == 8) {
--- app/controllers/malls_controller.rb
+++ app/controllers/malls_controller.rb
@@ -2,19 +2,29 @@ class MallsController < ApplicationController
   autocomplete :mall, :name, :full => true
 
   def index
-    @mall = Mall.new
-    @mall1 = Mall.find(1)
-    @mall2 = Mall.find(2)
-    if params[:search]
-      @mall = Mall.where(['name LIKE ?', "%#{params[:term]}%"])
-
+    if params[:mall]
+      @mall = Mall.where(['name LIKE ?', "%#{params[:mall][:name]}%"]).first
+      block_id_string = 'mall_block_id_' + params[:block_id]
+      session[block_id_string] = @mall.id
       respond_to do |format|
-        format.html
-        format.js  { render }
+        format.js { render locals: {block_id: params[:block_id]}}
+      end
+    else
+      @mall = Mall.new
+      if !session[:mall_block_id_1]
+        @mall1 = Mall.find(1)
+        session[:mall_block_id_1] =  @mall1.id
+      else
+        @mall1 = Mall.find(session[:mall_block_id_1])
+      end
+      if !session[:mall_block_id_2]
+        @mall2 = Mall.find(2)
+        session[:mall_block_id_2] =  @mall2.id
+      else
+        @mall2 = Mall.find(session[:mall_block_id_2])
       end
     end
 
   end
-
 
 end
